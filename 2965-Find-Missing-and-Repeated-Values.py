1a6a44c9ca3bcc75ea5fd1b32626d9f8c2748607
anjali2600-hub <anjaliagrawal2600@gmail.com>
2026-08-21 10:19:15 -0700

    Update 2965-Find-Missing-and-Repeated-Values.py

---
+++ 2965-Find-Missing-and-Repeated-Values.py
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int>ans;
+        unordered_set<int > s;
+        int n=grid.size();
+        int a,b;
+        int act_sum=0,exp_sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                act_sum+=grid[i][j];
+                if(s.find(grid[i][j])!=s.end())
+                {
+                    a=grid[i][j];
+                    ans.push_back(a);
+
+                }
+                s.insert(grid[i][j]);
+
+
+            }
+        }
+        exp_sum=(n*n)*((n*n)+1)/2;
+        b=exp_sum+a-act_sum;
+        ans.push_back(b);
+        return ans;
+    }
+};
